0b53fd30c7c2d580fb52add471e59bcfcc0477fb
mminzwoo4155 <minzwoo1234@gmail.com>
2023-04-29 15:27:49 +0700

    update view

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,3 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      Developed By Ang Kosal
-    </div>
-    <strong>&copy; <?php echo date('Y')?> - Point of Sale System</strong> - PHP Laravel
+    <strong>&copy; <?php echo date('Y')?> - Pharmacy Management System</strong> - PHP Laravel
   </footer>
